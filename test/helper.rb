11107d6ebe66008ebf83dab947eef42a704d89e4
Yusuke Endoh <mame@tsg.ne.jp>
2010-09-07 22:47:10 +0900

    Initial commit to inline-ook.

---
+++ test/helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'inline-ook'
+
+class Test::Unit::TestCase
+end
